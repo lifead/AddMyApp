e5be956782a6411a9c692482cb85ab73e140db50
vasilev-ev <vasilev-ev@speechpro.com>
2022-08-10 11:24:15 +0300

    Code CleanUp

--- AddMyApp/Controllers/HomeController.cs
+++ AddMyApp/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AddMyApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using AddMyApp.Models;
+using System.Diagnostics;
 
 namespace AddMyApp.Controllers
 {
--- AddMyApp/Models/ErrorViewModel.cs
+++ AddMyApp/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace AddMyApp.Models
 {
     public class ErrorViewModel
